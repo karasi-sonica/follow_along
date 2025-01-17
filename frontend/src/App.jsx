1ce72f231d8fb91696040d74c38021a2487f0ed2
karasi-sonica <karasisonica@gmail.com>
2025-01-17 20:33:54 +0530

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import { LoginPage } from "./Routes.jsx"
+import { SignupPage } from "./Routes.jsx"
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
 
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
 
     </Routes>
     </BrowserRouter>
