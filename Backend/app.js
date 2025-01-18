838aa69f2eb088cae4a6f0da1ace2a7154e0442c
karasi-sonica <karasisonica@gmail.com>
2025-01-18 10:40:08 +0530

    commited

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,26 @@
-const express=require("express");
-const app=express();
-const ErrorHandler = require('./utils/ErrorHandler');
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-// Config
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-if(process.env.NODE_ENV !== "PRODUCTION"){
+//Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
-        path:"Backend/config/.env"
-    })
-}
-app.use((err, req, res, next) => {
-    if(err instanceof ErrorHandler) {
-        //Custom error handling logic for ErrorHandler instance
-        return res.status(err.statusCode || 500).json({
-            message: err.message,
-            stack: err.stack
-        });
-    }
+        path: "./Backend/config/.env",
+    });
+};
 
-    //Default error handling if not an instance of ErrorHandler
-    res.status(500).json({ message: "Internal Server Error"});
-});
+//import Routes
+const user = require("./controller/user");
+app.use("/api/v2/user", user);
+
+//it's for ErrorHandling
+app.use(ErrorHandler);
 
 module.exports=app;
