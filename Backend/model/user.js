3b674eb65e87b58320bee0ea837928320d647b6c
karasi-sonica <karasisonica@gmail.com>
2025-01-20 12:26:41 +0530

    commited

--- Backend/model/user.js
+++ Backend/model/user.js
@@ -3,86 +3,87 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    name:{
+  name:{
+    type: String,
+    required: [true, "Please enter your name!"],
+  },
+  email:{
+    type: String,
+    required: [true, "Please enter your email!"],
+  },
+  password:{
+    type: String,
+    required: [true, "Please enter your password"],
+    minLength: [4, "Password should be greater than 4 characters"],
+    select: false,
+  },
+  phoneNumber:{
+    type: Number,
+  },
+  addresses:[
+    {
+      country: {
         type: String,
-        required: [true,"Please enter your name!"],
-    },
-    email:{
+      },
+      city:{
         type: String,
-        required: [true,"Please enter your email!"],
-    },
-    password:{
+      },
+      address1:{
         type: String,
-        required: [true,"Please enter your password!"],
-        minLength: [4, "Password should be greater than 4 characters"],
-        select: false,
-    },
-    phoneNumber:{
+      },
+      address2:{
+        type: String,
+      },
+      zipCode:{
         type: Number,
-    },
-    addresses:[
-        {
-            country: {
-                type: String,
-            },
-            city: {
-                type: String,
-            },
-            address1: {
-                type: String,
-            },
-            address2: {
-                type: String,
-            },
-            zipCode: {
-                type: Number,
-            },
-            addressType: {
-                type: String,
-            },
-        }
-    ],
-    role: {
+      },
+      addressType:{
         type: String,
-        default: "user",
-    },
-    avatar: {
-        public_id: {
-            type: String,
-            required: true,
-        },
-        url: {
-            type: String,
-            required: true,
-        },
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    resetPasswordToken: String,
-    resetPassswordTime: Date,
-});
-
-//Hash Password
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) {
-        next();
+      },
     }
-
-    this.password = await bcrypt.hash(this.password, 10);
+  ],
+  role:{
+    type: String,
+    default: "user",
+  },
+  avatar:{
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+ },
+ createdAt:{
+  type: Date,
+  default: Date.now(),
+ },
+ resetPasswordToken: String,
+ resetPasswordTime: Date,
 });
 
-//jwt token
+
+//  Hash password
+userSchema.pre("save", async function (next){
+  if(!this.isModified("password")){
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// jwt token
 userSchema.methods.getJwtToken = function () {
-    return JsonWebTokenError.sign({ id: this._id}, process.env.JWT_SECRET_KEY, {
-        expiresIn: process.env.JWT_EXPIRES,
-    });
+  return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
+    expiresIn: process.env.JWT_EXPIRES,
+  });
 };
 
-//Compare password
+// compare password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model("User", userSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,39 @@
 const app = require("./app");
 const connectDatabase = require("./db/Database");
 
-//Handling uncaught Exception when setting up backend server
+// Handling uncaught Exception when setting up backend server
 process.on("uncaughtException", (err) => {
-    console.log(`Error: $(err.message)`);
+    console.log(`Error: ${err.message}`);
     console.log(`shutting down the server for handling uncaught exception`);
-});
+  });
+  
 
-//Config
-
-if(process.env.NODE_ENV !== "PRODUCTION") {
+  // config
+if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
-        path: "config/.env",
+      path: "config/.env",
     });
 };
 
-//Connect DB
-
+// connect db
 connectDatabase();
 
-//create server
+
+// create server
 const server = app.listen(process.env.PORT, () => {
     console.log(
-        `Server is running on http://localhost:${process.env.PORT}`
+      `Server is running on http://localhost:${process.env.PORT}`
     );
-});
+  });
 
-//unhandled promise rejection 
-process.on("unhandledRejection", (err) => {
-    console.error(`unhandled Rejection: ${err.message}`);
+
+  // unhandled promise rejection(explain error handling when setting up server as you code)
+  process.on("unhandledRejection", (err) => {
+    console.error(`Unhandled Rejection: ${err.message}`);
     console.log("Shutting down the server due to unhandled promise rejection.");
-
+    
     server.close(() => {
-        process.exit(1); //Exit with failure code 
+      process.exit(1); // Exit with failure code
     });
-});
+  });
+  
